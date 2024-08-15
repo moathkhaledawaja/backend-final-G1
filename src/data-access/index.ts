dc5e2b6a3758cc2d888432502d77852c73a7a53c
Yousef Ali <yousefkmail169@gmail.com>
2024-08-15 21:02:30 +0300

    fix index files

--- src/data-access/index.ts
+++ src/data-access/index.ts
@@ -1,18 +1,14 @@
-import { Category, Comment, Product, User } from "../models";
+import { Cart, Category, Comment, Discount, Product, User } from "../models";
 import { CommentRepository } from "./CommentRepository";
 import { UserRepository } from "./UserRepository";
-import { IUserRepository } from "./Interfaces/IUserRepository";
-
 import { CartRepository } from "./CartRepository";
-import { ICartRepository } from "./Interfaces/ICartRepository";
-import { Cart } from "../models";
-import { ICommentRepository } from "./Interfaces/ICommentRepository";
 import { ProductRepository } from "./ProductRepository";
 import { CategoryRepository } from "./CategoryRepository";
+import { DiscountRepository } from "./DiscountRepository";
 
 export const userRepository = new UserRepository(User);
 export const cartRepository = new CartRepository(Cart);
 export const commentRepository = new CommentRepository(Comment);
 export const productRepository = new ProductRepository(Product);
 export const categoryRepository = new CategoryRepository(Category);
-export { IUserRepository, ICartRepository, ICommentRepository };
+export const discountCategpry = new DiscountRepository(Discount);
