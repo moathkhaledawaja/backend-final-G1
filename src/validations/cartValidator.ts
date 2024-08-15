cee5ea3c79d4ad73b805377cd388ef3c988161ad
NancyHamdallah <hamdallah.nancy@gmail.com>
2024-08-16 01:02:36 +0300

    edited file cartValidator

--- src/validations/cartValidator.ts
+++ src/validations/cartValidator.ts
@@ -1,25 +1,6 @@
 import { body, param } from 'express-validator'
 import { validateResult } from './validateResult'
 
-/* "Cart": {
-    "GetCart": {},
-    "DeleteCart": {},
-    "RemoveItem": {
-      "itemId": 1
-    },
-    "AddItem": {
-      "itemId": 1,
-      "quantity": 1
-    },
-    "IncreaseItem": {
-      "itemId": 1,
-      "amount": 1
-    },
-    "decreaseItem": {
-      "itemId": 1,
-      "amount": 1
-    } */
-
 export  const createCartValidator = [
    
         body("userId").isInt({ gt: 0 }).toInt(),
