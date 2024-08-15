a12c28b1e5e66979bd6381dab76353b3595a8503
Moath <mkaawaja5000@gmail.com>
2024-08-15 16:58:47 +0300

    create get all products

--- src/data-access/index.ts
+++ src/data-access/index.ts
@@ -1,8 +1,9 @@
-import { Comment, Product, User } from "../models";
+import { Category, Comment, Product, User } from "../models";
 import { CommentRepository } from "./CommentRepository";
 import { UserRepository } from "./UserRepository";
 import { IUserRepository } from "./Interfaces/IUserRepository";
-
+import { CategoryRepository } from "./CategoryRepository";
+import { ICategoryRepository } from "./Interfaces/ICategoryRepository";
 import { CartRepository } from "./CartRepository";
 import { ICartRepository } from "./Interfaces/ICartRepository";
 import { Cart } from "../models";
@@ -13,5 +14,6 @@ export const userRepository = new UserRepository(User);
 export const cartRepository = new CartRepository(Cart);
 export const commentRepository = new CommentRepository(Comment);
 export const productRepository = new ProductRepository(Product);
+export const categoryRepository = new CategoryRepository(Category);
 
-export { IUserRepository, ICartRepository, ICommentRepository };
+export { IUserRepository, ICartRepository, ICommentRepository, ICategoryRepository };
