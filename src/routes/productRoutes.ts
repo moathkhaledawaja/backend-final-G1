285078851459bcd340b2b576e839825e30f60212
Yousef Ali <yousefkmail169@gmail.com>
2024-08-16 21:45:48 +0300

    fix: replace findallproducts with GetProducts

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -9,10 +9,9 @@ const productController = container.resolve(ProductController);
 
 const router = Router();
 router.get("/list", productController.GetProducts.bind(productController));
-router.get("/all", productController.getAllProducts.bind(productController));
 router.get("/:id", productController.getProductById.bind(productController));
 router.post("/create", productController.createProduct.bind(productController));
-router.put("/:id", productController.updateProduct.bind(productController));
+router.patch("/:id", productController.updateProduct.bind(productController));
 router.delete("/:id", productController.deleteProduct.bind(productController));
 
 export default router;
