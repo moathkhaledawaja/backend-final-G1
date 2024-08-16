09c67a32b500cae83d8d9ebcb55730af627003b3
Yousef Ali <yousefkmail169@gmail.com>
2024-08-16 16:42:05 +0300

    fix: update all layers related to product

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -2,6 +2,7 @@ import ProductService from "../services/product.service";
 import { ProductDTO } from "../Types/DTO";
 import { injectable, inject } from "tsyringe";
 import { Request, Response } from "express";
+import { UpdateProductDTO } from "../Types/DTO/productDto";
 @injectable()
 export class ProductController {
   constructor(@inject(ProductService) private productService: ProductService) {}
@@ -16,63 +17,65 @@ export class ProductController {
       );
       res.status(200).json({ products });
     } catch (ex) {
-      console.log(ex);
+      return res
+        .status(500)
+        .json({ error: "internal server error, try again later." });
     }
   }
 
-  public async createProduct(req: Request, res: Response): Promise<void> {
+  public async createProduct(req: Request, res: Response) {
     try {
       const product: ProductDTO = req.body;
-      if (!product) {
-        res.status(500).json({ error: "Required Data is Unavailable" });
-      }
+
       const newProduct = await this.productService.createProduct(product);
-      if (!newProduct) {
-        res.status(400).json({ error: "error while creating new Product" });
-      }
-      res.status(201).json(newProduct.toJSON());
+
+      return res.status(201).json(newProduct);
     } catch (error: any) {
-      res.status(400).json({ error: error.message });
-      throw error;
+      return res
+        .status(500)
+        .json({ error: "internal server error, try again later." });
     }
   }
 
-  public async updateProduct(req: Request, res: Response): Promise<void> {
+  public async updateProduct(req: Request, res: Response) {
     try {
-      const newProduct: ProductDTO = req.body;
-      const productId = parseInt(req.params.id);
-      if (!newProduct || !productId) {
-        res.status(500).json({ error: "Required Data is Unavailable" });
-      }
-      const updatedProduct = await this.productService.updateProduct(
-        productId,
-        newProduct
+      const updatedData: UpdateProductDTO = req.body;
+
+      const updatedProduct = await this.productService.FindAndUpdateProduct(
+        updatedData.id,
+        updatedData
       );
+
       if (!updatedProduct) {
-        res.status(404).json({ error: "Error while updating category" });
+        return res
+          .status(404)
+          .json({ error: "could not find the product with the specified Id" });
       }
 
-      res.status(201).json(updatedProduct);
+      return res.status(201).json(updatedProduct);
     } catch (error: any) {
-      res.status(400).json({ error: error.message });
-      throw error;
+      return res
+        .status(500)
+        .json({ error: "internal server error, try again later." });
     }
   }
 
-  public async deleteProduct(req: Request, res: Response): Promise<void> {
+  public async deleteProduct(req: Request, res: Response) {
     try {
       const productId = parseInt(req.params.id);
       if (!productId) {
         res.status(500).json({ error: "Required Data is Unavailable" });
       }
-      await this.productService.deleteProduct(productId);
+      await this.productService.DeleteProduct(productId);
       res.status(201);
     } catch (error: any) {
-      res.status(500).json({ error: error.message });
+      return res
+        .status(500)
+        .json({ error: "internal server error, try again later." });
     }
   }
 
-  public async getProductById(req: Request, res: Response): Promise<void> {
+  public async getProductById(req: Request, res: Response) {
     try {
       const productId = parseInt(req.params.id, 10);
       if (!productId) {
@@ -84,22 +87,9 @@ export class ProductController {
       }
       res.status(201).json(product);
     } catch (error: any) {
-      res.status(500).json({ error: error.message });
-    }
-  }
-
-  public async getProductByName(req: Request, res: Response): Promise<void> {
-    try {
-      const { name } = req.body;
-      if (!name) {
-        res.status(500).json({ error: "Required Data is Unavailable" });
-      }
-      const product = await this.productService.findByName(name);
-      if (!product) {
-        res.status(404).json(product);
-      }
-    } catch (error: any) {
-      res.status(500).json({ error: error.message });
+      return res
+        .status(500)
+        .json({ error: "internal server error, try again later." });
     }
   }
 }
--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -10,11 +10,6 @@ const productController = container.resolve(ProductController);
 const router = Router();
 router.get("/list", productController.GetProducts.bind(productController));
 router.get("/:id", productController.getProductById.bind(productController));
-router.get("/name", productController.getProductByName.bind(productController));
-router.get(
-  "/categoryid/:id",
-  productController.getProductByName.bind(productController)
-);
 router.post("/create", productController.createProduct.bind(productController));
 router.put("/:id", productController.updateProduct.bind(productController));
 router.delete("/:id", productController.deleteProduct.bind(productController));
