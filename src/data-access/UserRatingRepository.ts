2405d5be15bbe967abea480f7ba84052fd8c4032
ibrahim hamshari <lomeseth1@gmail.com>
2024-08-18 19:50:42 +0300

    unified the userRating reop

--- src/data-access/UserRatingRepository.ts
+++ src/data-access/UserRatingRepository.ts
@@ -6,30 +6,10 @@ import { RepositoryBase } from "./RepositoryBase";
 
 export class UserRatingRepository extends RepositoryBase<UserRating> implements IUserRatingRepository {
   async findAllByProductId(productId: number): Promise<UserRating[] | null> {
-    try {
-      return await UserRating.findAll({ where: { productId: productId } });
-    } catch (ex: any) {
-      logger.error(ex);
-    }
-    return null;
+    return await UserRating.findAll({ where: { productId: productId } });
 
   }
   async findByUserIdAndProductId(userId: number, productId: number): Promise<UserRating | null> {
-    try {
-      return await UserRating.findOne({ where: { userId: userId, productId: productId } });
-    }
-    catch (ex: any) {
-      logger.error(ex);
-    }
-    return null;
-  }
-  findById(id: number): Promise<UserRating | null> {
-    throw new Error("Method not implemented.");
-  }
-  findAll(): Promise<UserRating[]> {
-    throw new Error("Method not implemented.");
-  }
-  delete(id: number): Promise<boolean> {
-    throw new Error("Method not implemented.");
+    return await UserRating.findOne({ where: { userId: userId, productId: productId } });
   }
 }
