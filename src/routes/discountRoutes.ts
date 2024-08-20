d05e6b3f257136a578891f3b8fb42adf4f76fb75
Yousef Ali <yousefkmail169@gmail.com>
2024-08-20 16:29:09 +0300

    fix: add admin auth middleware for adding a discount

--- src/routes/discountRoutes.ts
+++ src/routes/discountRoutes.ts
@@ -2,12 +2,14 @@ import { Router } from 'express'
 import { container } from 'tsyringe'
 
 import { DiscountController } from '../controllers'
+import authAndRoleMiddleware from '../middleware/authMiddleware'
 
 const discountRouter = Router()
 const discountController = container.resolve(DiscountController)
 
 discountRouter.post(
   '/Add',
+  authAndRoleMiddleware(['admin']),
   discountController.AddDiscount.bind(discountController)
 )
 
