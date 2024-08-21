a429363f70dca2e77a44f481a6821c60ead0dc5e
NancyHamdallah <hamdallah.nancy@gmail.com>
2024-08-22 00:47:58 +0300

    Added product validator

--- src/validations/productValidator.ts
+++ src/validations/productValidator.ts
@@ -20,15 +20,26 @@ export const createProductValidator = [
     .isString()
     .withMessage('Description must be a string'),
   body('brand').optional().isString().withMessage('Brand must be a string'),
-  /*  userRatings?: ratingDto[]
-  images?: Express.Multer.File[]
-  comments?: CommentDTO[]
-  categories?: CategoryDTO[]
-  discount?: discountDTO */
+
   validateResult,
 ]
 
-export const updateProductValidator = []
+export const updateProductValidator = [
+  body('id')
+    .notEmpty()
+    .withMessage('id is required')
+    .isInt()
+    .toInt()
+    .withMessage('id must be integer'),
+
+  body('name').optional().isString().withMessage('name must be a string'),
+  body('price').optional().isFloat().withMessage('price must be float number'),
+  body('description')
+    .optional()
+    .isString()
+    .withMessage('description must be string'),
+  body('stock').optional().isInt().toInt().withMessage('stock must be integer'),
+]
 
 export const deleteValidator = [
   param('id').isInt().toInt().withMessage('Id must be an integer'),
